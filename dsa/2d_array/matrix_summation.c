83d233f9f1a87c5a6f6bd11587c53da5abf42f6f
Zirconium001 <tan450878@gmail.com>
2026-09-05 21:08:06 +0600

    Update matrix_summation.c

--- dsa/2d_array/matrix_summation.c
+++ dsa/2d_array/matrix_summation.c
@@ -32,4 +32,42 @@ int main() {
         printf("\n");
     }
     return 0;
+
+
+
+    //user input
+    int a,b,c,d, i, j;
+    printf("Enter the order of 1st matrix: ");
+    scanf("%d %d", &a, &b);
+    printf("Enter the order of 2nd matrix: ");
+    scanf("%d %d", &c, &d);
+    int arr1[a][b], arr2[c][d], res[a][b];
+    if (a == c && b == d) {
+        printf("Enter the elements of the 1st matrix: \n");
+        for (i=0;i<a;i++) {
+            for (j=0;j<b;j++) {
+                scanf("%d", &arr1[i][j]);
+            }
+        }
+        printf("Enter the elements of the 2nd matrix: \n");
+        for (i=0;i<a;i++) {
+            for (j=0;j<b;j++) {
+                scanf("%d", &arr2[i][j]);
+            }
+        }
+        for (i=0;i<a;i++) {
+            for (j=0;j<b;j++) {
+                res[i][j] = arr1[i][j] + arr2[i][j];
+            }
+        }
+        for (i=0;i<a;i++) {
+            for (j=0;j<b;j++) {
+                printf("%d ", res[i][j]);
+            }
+            printf("\n");
+        }
+    }
+    else 
+        printf("Summation cannot be done!");
+    return 0;
 }
