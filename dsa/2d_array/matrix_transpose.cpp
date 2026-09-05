1e5c293a6628b25ca806f2f7acead7e35e048a61
Zirconium001 <tan450878@gmail.com>
2026-09-05 10:06:32 +0600

    Create matrix_transpose.cpp

---
+++ dsa/2d_array/matrix_transpose.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n,m;
+    cout<<"Enter the order of the matrix: ";
+    cin>>n>>m;
+    int matrix[n][m];
+    for (int i=0;i<n;i++) {
+        for (int j=0;j<m;j++) {
+            cin>>matrix[i][j];
+        }
+    }
+    for (int i=0;i<m;i++) {
+        for (int j=0;j<n;j++) {
+            cout<<matrix[j][i]<<" ";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
